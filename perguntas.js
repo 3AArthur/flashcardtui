168fc7a36fe679c1de09809f446014cadf17a333
3AArthur <00001110732612sp@al.educacao.sp.gov.br>
2024-11-25 15:50:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Professores',
+    'Qual o melhor professor do mundo?',
+    'Professor Caco'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem ganhou a bola de ouro deste ano?',
+    'Rodri, do Manchester City'
+)
+
+criaCartao(
+    'Historia',
+    'Que ano acabou a segunda Guerra Mundial?',
+    '1945'
+)
+
+criaCartao(
+    'Datas comemorativas',
+    'Que dia é o Natal?',
+    'Dia 25 de Dezembro'
+)
